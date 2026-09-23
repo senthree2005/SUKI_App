3312c45fe0da6bc4f86d749e6f448973d7d0fca6
SergeantMacNuggets <cd.cabz12@gmail.com>
2026-09-24 00:24:45 +0800

    manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import mkcert from 'vite-plugin-mkcert';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(),mkcert()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
@@ -15,6 +16,7 @@ export default defineConfig(() => {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not  watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
+      https:{} as any,
       // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
